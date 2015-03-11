1318f38541fc1cf959da85715a5400276af9fd6b
Nathan Miller <Nathan.J.Miller@nasa.gov>
2015-03-11 10:18:27 -0400

    Changed around plotting so that we can add graticules to the map. We now add HpxMollweideAxes to the figures instead on calling imshow() on the 2d projections.

--- footprint.py
+++ footprint.py
@@ -22,12 +22,10 @@ class SurveyStack(object):
 
 		polamp = np.sqrt(maps[0]**2 + maps[1]**2)
 
-		map2d = project_healpix_mollview(polamp,coord=['G','C'])
+		H.mollview(polamp,title='Experiment Footprints',xsize=1600,coord=['G','C'],fig=self.fig.number,cmap=cm.Greys,max=0.001,min=0.0,notext=True,cbar=None)
+		H.graticule(dpar=15.0,dmer=15.0,coord='C')
 
-		vmax_val = 0.80*np.max(map2d)
-
-		pl.imshow(map2d,cmap=cm.Greys,vmin=0.0,vmax=0.001,origin='lower')
-
+		#pl.show()
 
 	def superimpose(self,fns,color='red'):
 		'''Superimpose the footprint of an experiment on the background image. Can be a single fits file or a list of them that will 
@@ -48,29 +46,26 @@ class SurveyStack(object):
 			hdulist = pyfits.open(fn)
 			hpx_map += wcs_to_healpix(hdulist,self.nside)
 			hdulist.close()
+	
+		idx_nan = (hpx_map == 0)
+		idx_good = (hpx_map != 0)
 
-		hpx_map_2d = project_healpix_mollview(hpx_map)
-		
-		hpx_map_2d[hpx_map_2d == 0] = np.NaN
+		hpx_map[idx_nan] = np.NaN
 
 		if color == 'red':
 			color_val = 255.0
 		elif color == 'blue':
 			color_val = 0.0
 
-		hpx_map_2d[np.isfinite(hpx_map_2d)] = color_val
+		hpx_map[idx_good] = color_val
 
-		pl.figure(self.fig.number) #probably a better way to do this
-		pl.imshow(hpx_map_2d,alpha=0.5,origin='lower',vmin=0.0,vmax=255.0) #need to work on color. Possible just figure out values for the colormap
-
-def project_healpix_mollview(mapinp,coord=None):
-
-	#Change this to the minimal function calls needed to get the projection
-
-	map2d = H.mollview(mapinp,xsize=1600,coord=coord,return_projected_map=True)
-	pl.close()
-
-	return map2d
+		#Can't use mollview because it doesn't allow alpha so must create our own HpxMollweideAxes object and add it to the figure. 
+		#H.mollview(hpx_map,title='',xsize=1600,coord='C',fig=self.fig.number,min=0.0,max=255.0)
+		extent = (0.02,0.05,0.96,0.9)
+		cm.jet.set_bad(alpha=0.0) #so parts of the Nhits map that are NaN are completely transparent
+		ax = H.projaxes.HpxMollweideAxes(self.fig,extent,coord='C',rot=None,format='%g',flipconv='astro')
+		map2d = ax.projmap(hpx_map,xsize=1600,coord='C',vmin=0.0,vmax=255.0,alpha=0.5,cmap=cm.jet)
+		self.fig.add_axes(ax)
 
 def wcs_to_healpix(hdulist,nside):
 	'''Converts data in an opened FITS file from a generic WCS to Healpix'''
@@ -94,6 +89,7 @@ def wcs_to_healpix(hdulist,nside):
 	print "wcs_pix2world"
 	world = w.wcs_pix2world(pixcrd,1)
 
+	#needed since for some reason I was getting NaNs for some ACT data
 	idx = np.isfinite(world[:,0])
 
 	pixcrd2 = pixcrd2[idx,:]
@@ -101,7 +97,7 @@ def wcs_to_healpix(hdulist,nside):
 
 	#Use Healpy to do ang2pix for Healpix pixel numbers
 	theta = np.radians(world[:,0])
-	phi = -np.radians(world[:,1]) #CHECK SIGN
+	phi = np.radians(world[:,1]) #CHECK SIGN
 	print "ang2pix"
 	pixnum = H.ang2pix(nside,theta,phi)
 	
@@ -132,8 +128,10 @@ if __name__=='__main__':
 	fn_220_equ_s4 = 'maps/ACT/ACT_220_equ_season_4_1way_hits_v3.fits'
 	fn_220_south_s4 = 'maps/ACT/ACT_220_south_season_4_1way_hits_v3.fits'
 
-	fns_ACT = [fn_148_equ_s4,fn_148_south_s4,fn_148_equ_s3,fn_148_south_s3,fn_148_south_s2]
+	#fns_ACT = [fn_148_equ_s4,fn_148_south_s4,fn_148_equ_s3,fn_148_south_s3,fn_148_south_s2]
+	fns_ACT = [fn_148_equ_s4]
 
 	footprint.superimpose(fns_ACT,color='red')
-
+	
 	pl.show()
+
