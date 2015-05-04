dc895f71b58be61c0c81e8f672a44c99b9ec8bb1
Nathan Miller <Nathan.J.Miller@nasa.gov>
2015-05-04 15:40:02 -0400

    Cleaning up plot_footprint.py and moving the code that I used to generate some maps to a different script.

--- plot_footprint.py
+++ plot_footprint.py
@@ -14,27 +14,11 @@ if __name__ == '__main__':
     data = H.read_map(fn_background,field=(0,1))
     background_map = np.sqrt(data[0]**2 + data[1]**2)
 
-    fp = footprint.SurveyStack(background_map, fignum=1, projection=H.cartview, coord_plot='C')
+    #fp = footprint.SurveyStack(background_map, fignum=1, projection=H.cartview, coord_plot='C', rot=[0,-32.5], 
+    #        partialmap=True, latra=[-20,20], lonra=[-20,20])
+    fp = footprint.SurveyStack(background_map, fignum=1, projection=H.mollview, coord_plot='C', rot=[0,0]) 
 
-#   fn_148_south_s2 = 'maps/ACT/ACT_148_south_season_2_1way_hits_v3.fits'
-    fn_148_equ_s3 = 'maps/ACT/ACT_148_equ_season_3_1way_hits_v3.fits'
-#   fn_148_south_s3 = 'maps/ACT/ACT_148_south_season_3_1way_hits_v3.fits'
-#   fn_148_equ_s4 = 'maps/ACT/ACT_148_equ_season_4_1way_hits_v3.fits'
-#   fn_148_south_s4 = 'maps/ACT/ACT_148_south_season_4_1way_hits_v3.fits'
+    fp.superimpose_experiment('BOSS-LSS-RANDOM2',color='orange')
 
-#   fn_220_equ_s4 = 'maps/ACT/ACT_220_equ_season_4_1way_hits_v3.fits'
-#   fn_220_south_s4 = 'maps/ACT/ACT_220_south_season_4_1way_hits_v3.fits'
-
-    fn_148_spt = 'maps/SPT/spt_wgt_ra5h30dec-55_2008_150ghz_zea_dr1.fits'
-
-#   fns_ACT = [fn_148_equ_s4, fn_148_south_s4, fn_148_equ_s3, fn_148_south_s3,
-#              fn_148_south_s2]
-    fns_ACT = [fn_148_equ_s3]
-    fns_SPT = [fn_148_spt]
-
-#   fp.superimpose_fits(fns_SPT,color='red',maptype='WCS')
-#   footprint.superimpose_boundary_cen((0,0),(40,5),color='blue')
-    fp.superimpose_experiment('ACT', color='green')
-    fp.superimpose_experiment('SPT', color='red')
-
+    
     pl.show()
