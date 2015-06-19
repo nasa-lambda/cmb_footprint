f8d711950e49d6d7d0bb2a5660bc495ee99e14cc
Nathan Miller <Nathan.J.Miller@nasa.gov>
2015-06-19 15:27:32 -0400

    Changing DustPol background entry name and adding a Dust temperature background.

--- examples/plot_cmbpolarization.py
+++ examples/plot_cmbpolarization.py
@@ -21,11 +21,9 @@ if __name__ == '__main__':
 #   data = H.read_map(fn_background,field=(0,1))
 #   background_map = np.sqrt(data[0]**2 + data[1]**2)
 
-#   fp = footprint.SurveyStack(background_map, fignum=1, projection=H.cartview, coord_plot='C', rot=[0,-32.5], 
-#           partialmap=True, latra=[-20,20], lonra=[-20,20], config='../footprint.cfg')
 #   fp = footprint.SurveyStack(background_map, fignum=1, projection=H.mollview, coord_plot='C', rot=[0,0],
 #                              config='../footprint.cfg') 
-    fp = footprint.SurveyStack('PLANCK-DUSTPOL-AMP', fignum=1, projection=H.mollview, coord_plot='C', rot=[0,0],
+    fp = footprint.SurveyStack('PLANCK-DUSTPOL', fignum=1, projection=H.mollview, coord_plot='C', rot=[0,0],
                                config='../footprint.cfg') 
 
     fp.superimpose_experiment('ACT',color='green')
--- examples/plot_lss.py
+++ examples/plot_lss.py
@@ -16,14 +16,13 @@ from cmb_footprint import footprint
 
 if __name__ == '__main__':
 
-    #fn_background = '../maps/Planck/COM_CompMap_DustPol-commander_1024_R2.00.fits'
-    #data = H.read_map(fn_background,field=(0,1))
-    #background_map = np.sqrt(data[0]**2 + data[1]**2)
-    #fp = footprint.SurveyStack(background_map, fignum=1, projection=H.cartview, coord_plot='C', rot=[0,-32.5], 
-    #        partialmap=True, latra=[-20,20], lonra=[-20,20])
-    #fp = footprint.SurveyStack(background_map, fignum=1, projection=H.mollview, coord_plot='C', rot=[0,0]) 
+#   fn_background = '../maps/Planck/COM_CompMap_DustPol-commander_1024_R2.00.fits'
+#   data = H.read_map(fn_background,field=(0,1))
+#   background_map = np.sqrt(data[0]**2 + data[1]**2)
+#   fp = footprint.SurveyStack(background_map, projection=H.mollview,
+#                              coord_plot='C') 
 
-    fp = footprint.SurveyStack('PLANCK-DUSTPOL-AMP', fignum=1, projection=H.mollview, coord_plot='C', rot=[0,0],
+    fp = footprint.SurveyStack('PLANCK-DUSTPOL', projection=H.mollview, coord_plot='C', rot=[0,0],
                                config='../footprint.cfg') 
 
     fp.superimpose_experiment('BOSS-LSS-RANDOM2',color='green')
