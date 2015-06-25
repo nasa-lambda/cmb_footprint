2422ebfb5fa70fcdd1e1b7328859ef0268d8c5cc
Nathan Miller <Nathan.J.Miller@nasa.gov>
2015-06-25 16:18:53 -0400

    Adding SPIDER entries. Adding different examples for CMB polarization surveys and CMB temperature surveys.

--- examples/plot_cmbtemperature.py
+++ examples/plot_cmbtemperature.py
@@ -23,12 +23,10 @@ if __name__ == '__main__':
 
 #   fp = footprint.SurveyStack(background_map, fignum=1, projection=H.mollview, coord_plot='C', rot=[0,0],
 #                              config='../footprint.cfg') 
-    fp = footprint.SurveyStack('PLANCK-DUSTPOL', fignum=1, projection=H.mollview, coord_plot='C', rot=[0,0],
+    fp = footprint.SurveyStack('PLANCK-DUST', fignum=1, projection=H.mollview, coord_plot='C', rot=[0,0],
                                config='../footprint.cfg') 
 
-    fp.superimpose_experiment('POLARBEAR',color='magenta')
-    fp.superimpose_experiment('BICEP2',color='blue')
-    fp.superimpose_experiment('SPIDER-90',color='orange')
-    fp.superimpose_experiment('QUIET',color='green')
+    fp.superimpose_experiment('ACT',color='green')
+    fp.superimpose_experiment('SPT',color='red')
 
     pl.show()
